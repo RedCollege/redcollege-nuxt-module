3076f9a838eca6da52bb48e6d9a4b69f03ab5716
José Puma <45923235+josepuma@users.noreply.github.com>
2024-09-04 09:22:53 -0400

    Add @vee-validate/nuxt

--- src/runtime/composables/states.ts
+++ src/runtime/composables/states.ts
@@ -2,11 +2,9 @@ import { useSidebarStore } from '../stores/sidebarStore'
 import { useAuthStore } from '../stores/authStore'
 import { useNavbarStore } from '../stores/navbarStore'
 import { useToast } from '../components/ui/toast/'
-import { useForm as useVeeForm } from 'vee-validate'
 
 export const useSidebar = () => useSidebarStore()
 export const useAuth = () => useAuthStore()
 export const useNavbar = () => useNavbarStore()
 
 export const useNotification = () => useToast()
-export const useForm = () => useVeeForm()
