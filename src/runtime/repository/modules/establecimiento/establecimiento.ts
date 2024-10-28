bff65a5afcdb3529097395ed5617bcc634c5ae68
Dayron_Glez <dayronglez01@gmail.com>
2024-10-28 15:57:23 -0400

    feat(establecimiento): Agregar metodo para obtener los colaboradores de un establecimiento

--- src/runtime/repository/modules/establecimiento/establecimiento.ts
+++ src/runtime/repository/modules/establecimiento/establecimiento.ts
@@ -1,6 +1,7 @@
 import type { $Fetch } from 'ofetch';
 import type { ICursoAsignatura } from '../../../models/Establecimiento';
 import type { ICurso } from '~/src/runtime/models/Establecimiento/curso';
+import type { IUsuario } from '~/src/runtime/models/Auth/usuario';
 
 
 
@@ -9,7 +10,7 @@ export default class EstablecimientoModule {
     constructor(private fetcher: $Fetch) { }
 
 
-    async getAsignaturasByCurso(cursoId: number): Promise<ICursoAsignatura> {
+    async getAsignaturasByCurso(cursoId: number): Promise<ICursoAsignatura[]> {
         return this.fetcher(`/establecimiento/curso/${cursoId}/asignaturas`, {
             method: 'GET'
         });
@@ -19,6 +20,13 @@ export default class EstablecimientoModule {
         return this.fetcher(`/establecimiento/${establecimientoId}/cursos`, {
             method: 'GET',
             params: { periodo, isDinamico }
+        });
+    }
+
+    async getColaboradoresByEstablecimiento(establecimientoId: number): Promise<IUsuario[]> {
+        return this.fetcher(`/establecimiento/${establecimientoId}/personal`, {
+            method: 'GET',
+
         });
     }
 
