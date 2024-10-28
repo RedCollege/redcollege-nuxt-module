7c4ca1e6007a993b8d75809e88e5ac223dd79c7b
Dayron_Glez <dayronglez01@gmail.com>
2024-10-27 20:21:43 -0400

    feat(establecimiento): Agregado metodo para obtener los cursos de un establecimiento por año

--- src/runtime/repository/modules/establecimiento/establecimiento.ts
+++ src/runtime/repository/modules/establecimiento/establecimiento.ts
@@ -1,16 +1,23 @@
 import type { $Fetch } from 'ofetch';
-import { ICursoAsignatura } from '../../../models/Establecimiento';
+import type { ICursoAsignatura } from '../../../models/Establecimiento';
 
 
 
 
 export default class EstablecimientoModule {
-    constructor(private fetcher: $Fetch) {}
+    constructor(private fetcher: $Fetch) { }
 
 
-async getAsignaturasByCurso(cursoId: number): Promise<ICursoAsignatura> {
+    async getAsignaturasByCurso(cursoId: number): Promise<ICursoAsignatura> {
         return this.fetcher(`/establecimiento/curso/${cursoId}/asignaturas`, {
             method: 'GET'
+        });
+    }
+
+    async getAllCursosByEstablecimiento(establecimientoId: number, periodo: number): Promise<ICursoAsignatura[]> {
+        return this.fetcher(`/establecimiento/${establecimientoId}/cursos`, {
+            method: 'GET',
+            params: { periodo }
         });
     }
 
