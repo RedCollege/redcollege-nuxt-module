57975fcaceae290b457e27997bb7a0be60c3550a
José Puma <45923235+josepuma@users.noreply.github.com>
2024-11-12 19:44:16 -0300

    fix(api_factory) Cambiar momento donde se lee el token del usuario

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
     modules: ['../src/module'],
     ssr: false,
     redcollege: {
-        baseURL: 'http://localhost:3333/api/v3',
+        baseURL: 'http://localhost:3334/api/v3',
         logoURL: 'https://redcollege.nyc3.digitaloceanspaces.com/docs/components/planificaciones-logo.svg',
         nombreModulo: 'Planificaciones'
     },
--- playground/pages/[establecimiento-id].vue
+++ playground/pages/[establecimiento-id].vue
@@ -1,9 +1,17 @@
 <script lang="ts" setup>
+
+const { $apis } = useNuxtApp()
+
+const { data } = await useAsyncData('planificaciones', () => {
+    return $apis.planificaciones.planificacion.getAll()
+})
+
 useNavbar().setHideCursos(true)
 </script>
 
 <template lang="pug">
 NuxtLayout(name="dashboard")
     .w-32.h-32.bg-card-foreground
+    pre {{ data.data }}
     div {{ useRoute().params }}
 </template>
