5282faca440608dbd94dba7a91011470e3ba6c30
marcosdgi <marcosdguilarte74@gmail.com>
2024-11-13 08:24:00 -0500

    FIX(repository): Arreglar ruta para consumir niveles sige

--- src/runtime/repository/modules/establecimiento/curso.ts
+++ src/runtime/repository/modules/establecimiento/curso.ts
@@ -5,8 +5,8 @@ import type { ICursoAsignatura, IGradosSige } from '../../../models';
 export default class CursoModule {
     constructor(private fetcher: $Fetch) { }
 
-    async getGradosSige(establecimientoId: number): Promise<IGradosSige[]> {
-        return this.fetcher(`/curso/obtenerNivelesSige/${establecimientoId}`, {
+    async getGradosSige(establecimientoId: number, year: number): Promise<IGradosSige[]> {
+        return this.fetcher(`/curso/obtenerNivelesSige/${establecimientoId}/?year=${year}`, {
             method: 'GET'
         })
     }
