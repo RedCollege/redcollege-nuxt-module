0822e3c679b07c8863fe02d7b018170fafb15d70
marcosdgi <marcosdguilarte74@gmail.com>
2024-12-26 10:32:24 -0500

    FIX(repository): exportar RegistroDuaModula

--- src/runtime/repository/modules/index.ts
+++ src/runtime/repository/modules/index.ts
@@ -1,10 +1,10 @@
 export { default as PlanificacionModule } from './planificaciones/planificacion';
 export { default as InformesModule } from './informes/informes';
 export { default as UnidadModule } from './planificaciones/unidad'
-export { default as MatriculaModule } from './matricula/matricula'
+export { default as MatriculaModule } from './matricula/matricula'
 export { default as ItemEntradaCurricularModule } from './curriculum/item_entrada_curricular'
 export { default as ContenidoModule } from './planificaciones/contenido'
 export { default as ContenidoActividadModule } from './planificaciones/contenido_actividad'
 export { default as ContenidoRecursoModule } from './planificaciones/contenido_recurso'
-
+export { default as RegistroDuaModule } from './planificaciones/dua/registro_dua'
 export { default as HealthModule } from './health/health'
