364336b9f2269b4260995bd610f5a56d35a4b147
José Puma <45923235+josepuma@users.noreply.github.com>
2025-01-10 20:42:01 -0300

    fix transmit composable

--- playground/pages/[establecimiento-id]/[periodo]/transmit.vue
+++ playground/pages/[establecimiento-id]/[periodo]/transmit.vue
@@ -3,17 +3,18 @@
     const { transmit } = useTransmit()
 
     onMounted(async () => {
-        try{
-            const subscription = transmit.subscription('channelName')
-            await subscription.create()
+        const subscription = transmit.subscription('review')
+        console.log('subscription creating...')
 
-            subscription.onMessage((data) => {
-                console.log(data);
-            });
-        }catch(error) {
+        await subscription.create().then((event) => {
+        console.log('subscription created', event)
+        })
 
-        }
+        console.log('starting to listen for messages...')
 
+        subscription.onMessage((message) => {
+        console.log(message, '<<< came here')
+        })
     })
 
 </script>
--- src/runtime/composables/useTransmit.ts
+++ src/runtime/composables/useTransmit.ts
@@ -1,12 +1,30 @@
-import { useRuntimeConfig } from "#app";
+
 import { onMounted } from "vue";
 import { Transmit } from '@adonisjs/transmit-client'
+import { useRuntimeConfig } from "#app";
 const { socketURL } = useRuntimeConfig().public.redcollege
 
 export const useTransmit = () => {
-    let transmit = new Transmit({
-        baseUrl: socketURL
+    const transmit = new Transmit({
+        baseUrl: "http://localhost:3334"
     })
+
+    transmit.on('connected', (event) => {
+        console.log('connected to the server', event)
+    })
+
+    transmit.on('disconnected', () => {
+        console.log('disconnected')
+    })
+
+    transmit.on('initializing', (event) => {
+        console.log('initializing the server', event)
+    })
+
+    transmit.on('reconnecting', (error) => {
+        console.log(error)
+    })
+
     return {
         transmit
     }
