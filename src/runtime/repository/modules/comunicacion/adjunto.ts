ed5cc3179c7d90bc65318560feb4d7d462317e8d
Dayron_Glez <dayronglez01@gmail.com>
2025-01-28 11:54:31 -0500

    feat: Agregar metodos para subir y eliminar adjuntos en comunicaciones

--- src/runtime/repository/modules/comunicacion/adjunto.ts
+++ src/runtime/repository/modules/comunicacion/adjunto.ts
@@ -1,6 +1,29 @@
-import type { $Fetch } from 'ofetch';
-
+import type { $Fetch } from "ofetch";
+import type { IAdjunto } from "~/src/runtime/models";
 
 export default class AdjuntoModule {
-    constructor(private fetcher: $Fetch) { }
+    constructor(private fetcher: $Fetch) {}
+
+    async subirAdjunto(file: File, folder: string): Promise<IAdjunto> {
+        const formData = new FormData();
+        formData.append("file", file);
+        formData.append("folder", folder);
+
+        return this.fetcher(`comunicacion/adjuntos/uploadAdjunto`, {
+            method: "POST",
+            body: formData,
+        });
+    }
+
+    async eliminarAdjunto(
+        url: string,
+        id: number
+    ): Promise<{ success: boolean; message: string }> {
+        return this.fetcher(`comunicacion/adjuntos/deleteAdjunto${id}`, {
+            method: "DELETE",
+            body: {
+                url,
+            },
+        });
+    }
 }
