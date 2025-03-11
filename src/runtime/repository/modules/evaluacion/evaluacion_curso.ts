5a14eb706c5940c0d9c8d7cfd922a3ae8e5f6cbb
José Puma <45923235+josepuma@users.noreply.github.com>
2025-03-11 15:16:45 -0300

    Update evaluacion_curso.ts

--- src/runtime/repository/modules/evaluacion/evaluacion_curso.ts
+++ src/runtime/repository/modules/evaluacion/evaluacion_curso.ts
@@ -5,7 +5,7 @@ import type { ICursoEvalua } from '~/src/runtime/models/Evaluacion';
 export default class EvaluacionCursoModule {
     constructor(private fetcher: $Fetch) { }
 
-    async getAsignaturaCurricularSige(establecimientoId: number, periodo: number): Promise<Array<ICursoEvalua>> {
+    async obtenerInformeCursoByEstablecimientoId(establecimientoId: number, periodo: number): Promise<Array<ICursoEvalua>> {
         return this.fetcher(`/evaluacion_curso/obtenerInformeCursoByEstablecimientoId/${establecimientoId}?periodo=${periodo}`, {
             method: 'GET'
         })
