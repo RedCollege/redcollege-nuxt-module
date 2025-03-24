328aa2a7acf5ea2220f30a3c74c1f6d0916f2121
Eduardo Javier Garcia Alfonso <eduardojga.work@gmail.com>
2025-03-24 18:42:32 -0400

    fix(enfermeria): actualizar getAllConfiguracionItemByEstablecimiento para manejar la prop context si es array

--- src/runtime/repository/modules/enfermeria/configuracion_item.ts
+++ src/runtime/repository/modules/enfermeria/configuracion_item.ts
@@ -1,18 +1,27 @@
 import type { $Fetch } from 'ofetch';
-import type { IConfiguracionItem, IConfiguracionItemForm } from '~/src/runtime/models';
+import type { ContextEnum, IConfiguracionItem, IConfiguracionItemForm } from '~/src/runtime/models';
 
 interface Props {
     establecimientoId : string | number
-    context: string 
+    context: string | string[]
 }
 
 export default class ConfiguracionItemModule{
     constructor(private fetcher: $Fetch) { }
  
     async getAllConfiguracionItemByEstablecimiento({establecimientoId, context }: Props): Promise<IConfiguracionItem[]> {
+
+        const params: Record<string, any> = {}
+
+        if (Array.isArray(context)) {
+            params.context = context.map(c => c)
+        } else {
+            params.context = context
+        }
+
         return this.fetcher(`/enfermeria/configuracion-item/${establecimientoId}`, {
             method: 'GET',
-            params: { context },
+            params,
         });
     }
 
