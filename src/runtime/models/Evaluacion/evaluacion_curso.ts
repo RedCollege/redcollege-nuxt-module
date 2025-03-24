fbb3de04beace095b1dab92abc88b5573bf3cfc2
José Puma <45923235+josepuma@users.noreply.github.com>
2025-03-24 16:27:50 -0300

    Update evaluacion_curso.ts

--- src/runtime/models/Evaluacion/evaluacion_curso.ts
+++ src/runtime/models/Evaluacion/evaluacion_curso.ts
@@ -143,6 +143,7 @@ export interface IAsignaturaEstablecimientoComponenteEvalua {
 
 export interface IItemEstablecimientoEvalua {
     item_id: number;
+    item_alias: string;
     nombre: string;
     establecimientos: IEstablecimientoItemEvalua[];
 }
