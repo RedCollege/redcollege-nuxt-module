787b4df365ee15f7e6bb03a1dce1eb6390905a64
EndYou01 <jorgesenjudo@gmail.com>
2025-03-28 13:04:35 -0400

    fix(planificacion) cambiando el nombre de la propiedad ContenidosAsociados a cantidadContenidosAsociados

--- src/runtime/models/Planificacion/unidad.ts
+++ src/runtime/models/Planificacion/unidad.ts
@@ -10,7 +10,7 @@ export interface IUnidad {
     planificacionId: number;
     titulo: string;
     numero: number;
-    contenidosAsociados: number;
+    cantidadContenidosAsociados: number;
     descripcion: string | null;
     fechaInicio: DateTime | null | string;
     fechaFin: DateTime | null | string;
