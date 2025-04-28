3baf308d582428d147e94e99fa6831cef999e352
José Puma <45923235+josepuma@users.noreply.github.com>
2025-04-28 10:59:50 -0400

    add carousel component

---
+++ playground/pages/[establecimientoid]/[periodo]/carousel.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+    Card
+        CardHeader
+            CardTitle Carousel
+        CardContent
+            Carousel.relative.w-full(:opts="{ align: 'start' }")
+                CarouselContent
+                    CarouselItem(v-for="i in 6" class="md:basis-1/2 lg:basis-1/4")
+                        Card
+                            CardContent.flex.aspect-square.items-center.justify-center.p-6
+                                .text-4xl.font-semibold {{ i }}
+                CarouselPrevious
+                CarouselNext
+</template>
