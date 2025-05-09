c8e4dc4f6ad948b89ea0d72e8c1dd41e39f3e7cb
Dayron_Glez <dayronglez01@gmail.com>
2025-05-09 09:51:28 -0400

    feat: Agregar el variant gold para el progress

--- src/runtime/components/ui/progress/index.ts
+++ src/runtime/components/ui/progress/index.ts
@@ -1,25 +1,23 @@
-import {cva, type VariantProps} from 'class-variance-authority'
+import { cva, type VariantProps } from 'class-variance-authority'
 
 export { default as Progress } from './Progress.vue'
-export const progressVariants = cva(
-    'items-center',
-    {
-        variants: {
-            variant: {
-                default: 'bg-muted',
-                success: 'bg-green',
-                sky: 'bg-sky',
-                destructive: 'bg-destructive',
-                orange: 'bg-orange',
-                primary: 'bg-primary',
-                purple: 'bg-purple',
-                yellow: 'bg-yellow'
-            }
-        },
-        defaultVariants: {
-            variant: 'success'
+export const progressVariants = cva('items-center', {
+    variants: {
+        variant: {
+            default: 'bg-muted',
+            success: 'bg-green',
+            sky: 'bg-sky',
+            destructive: 'bg-destructive',
+            orange: 'bg-orange',
+            primary: 'bg-primary',
+            purple: 'bg-purple',
+            yellow: 'bg-yellow',
+            gold: 'bg-gold'
         }
     },
-)
+    defaultVariants: {
+        variant: 'success'
+    }
+})
 
 export type ProgressVariant = VariantProps<typeof progressVariants>
