7d0728978f4bf2107a14f2cff7773242d8607244
José Puma <45923235+josepuma@users.noreply.github.com>
2025-06-13 18:47:12 -0500

    add (models) Agregar modelo de curso_asignatura_curricular

---
+++ src/runtime/models/Establecimiento/curso_asignatura_curricular.ts
@@ -0,0 +1,26 @@
+import { DateTime } from 'luxon';
+import type { IAsignaturaCurricular } from "./asignatura_curricular.ts";
+import type { IAsignaturaSige } from "./asignatura_sige.ts";
+
+export interface ICursoAsignaturaCurricular {
+    id: number;
+    cursoId: number;
+    asignaturaId: number;
+    notaAPromedio: boolean;
+    notaConEscala: boolean;
+    notaAllInforme: boolean;
+    parentAsignaturaId: number | null;
+    isPonderacionSimple: boolean;
+    posicion: number | null;
+    notaCalculable: boolean;
+    tipoEvaluacionId: number | null;
+    asignaturaSigeId: number | null;
+    alias: string | null;
+    color: string | null;
+    reglaPromocion: string | null;
+    createdAt: DateTime;
+    updatedAt: DateTime;
+    asignaturas: Array<ICursoAsignaturaCurricular>;
+    asignaturaCurricular: IAsignaturaCurricular;
+    asignaturaSige: IAsignaturaSige;
+}
