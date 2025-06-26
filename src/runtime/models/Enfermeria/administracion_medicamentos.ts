016a59aeee2d20c295b19672daee6e613b4600c9
Eduardo J. Pimienta Alfonso <eduardojga.work@gmail.com>
2025-06-26 03:19:37 -0400

    feat(enfermeria): agregar boolean de pagiante a los filtros de administracion de medicamentos

--- src/runtime/models/Enfermeria/administracion_medicamentos.ts
+++ src/runtime/models/Enfermeria/administracion_medicamentos.ts
@@ -64,6 +64,7 @@ export interface IAdministracionMedicamentoFilter {
     paciente?: number;
     responsable?: number;
     mes?: string | Date;
+    paginate?: boolean;
 }
 
 export interface IAdministracionMedicamentoAdjunto {
