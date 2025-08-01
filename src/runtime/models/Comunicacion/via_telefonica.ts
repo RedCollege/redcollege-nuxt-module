ca585596d10e8b50544a42aae9b51f3f4cb8dbe0
Dayron_Glez <dayronglez01@gmail.com>
2025-08-01 11:41:23 -0400

    FEAT: Crear modelo de via_telefonica y los metodos necesarios

--- src/runtime/models/Comunicacion/via_telefonica.ts
+++ src/runtime/models/Comunicacion/via_telefonica.ts
@@ -3,7 +3,21 @@ import type { IUsuario } from "../Auth/usuario.ts";
 import type { IRegistroAtencion } from "../Enfermeria/registro_atencion.ts";
 import type { IRegistroAccidente } from "../Enfermeria/registro_accidente.js";
 import type { IAdministracionMedicamento } from "../Enfermeria/administracion_medicamentos.js";
-        
+
+export enum TipoRegistro {
+    ATENCION = 'atencion',
+    ACCIDENTE = 'accidente', 
+    ADMINISTRACION_MEDICAMENTO = 'administracion_medicamento'
+}
+
+export interface IViaTelefonicaForm {
+    fechaReporte: DateTime;
+    isContactoEstablecido: boolean;
+    observaciones: string;
+    tipoRegistro: TipoRegistro;
+    apoderados: Array<number>;
+}
+
 export interface IViaTelefonica {
     id: number;
     fechaReporte: DateTime;
