26e9571b8ef9a16fef3c8a6e9bc83d933746d20c
srantoan <antoanbarrera01@gmail.com>
2025-08-19 11:09:32 -0400

    fix: toast modificado para mostrar un preview de la notificación recibida

--- src/runtime/components/Notificaciones/Card.vue
+++ src/runtime/components/Notificaciones/Card.vue
@@ -38,7 +38,7 @@ const marcarComoLeidoRedireccion = async () => {
 .flex.flex-row.items-center.p-2.pr-4.gap-4.px-6(:class="`border-y border-y-muted-foreground/5 cursor-pointer hover:bg-primary/10 transition-colors duration-100 ease-in-out ${!props.notificacion.isLeido ? 'bg-primary/5 ' : 'bg-transparent'}`" @click="marcarComoLeidoRedireccion")
     .relative
         Avatar(class="h-14 w-14 bg-muted border border-muted-foreground/20 shadow-xl")
-            AvatarImage(:src="notificacion.emisor.avatarUrl")
+            AvatarImage(:src="String(notificacion.emisor.avatarUrl)")
             AvatarFallback(class="text-xl") {{ notificacion.emisor.iniciales }}
         img(:src="notificacion.tipo.icono" class="border shadow-xl absolute w-6 h-6 rounded-full bg-white p-1 -bottom-1 -right-1")
 
